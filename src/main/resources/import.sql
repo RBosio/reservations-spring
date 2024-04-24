bb9a35aace4476a4e95607fb065fde98019580db
Fiido <roccobosio@gmail.com>
2024-04-24 11:12:46 -0300

    default data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO customer(dni, full_name, credit_card, phone_number) VALUES('12345678A', 'John Doe', '1234567890123456', '123456789');
+
+INSERT INTO fly(destiny_lat, destiny_lng, origin_lat, origin_lng, price, destiny_name, origin_name, aero_line) VALUES(40.416775, -3.703790, 41.385063, 2.173404, 100, 'Madrid', 'Barcelona', 'aero_gold');
+INSERT INTO fly(destiny_lat, destiny_lng, origin_lat, origin_lng, price, destiny_name, origin_name, aero_line) VALUES(40.416775, -3.703790, 51.507351, -0.127758, 200, 'Madrid', 'London', 'aero_gold');
+
+INSERT INTO hotel(price, rating, address, name) VALUES(100, 5, 'Calle de la Princesa, 27, 28008 Madrid', 'Hotel Ritz');
+INSERT INTO hotel(price, rating, address, name) VALUES(200, 4, 'Calle de la Princesa, 27, 28008 Madrid', 'Hotel Ritz');
